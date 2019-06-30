c19936ecf312c17185b42febafc26b1ee4a1e05d
Jonathan King <imjonathanking@gmail.com>
2019-06-30 18:16:42 -0400

    Fixed the login issue for real.  All hail mongod

--- models/User.js
+++ models/User.js
@@ -48,12 +48,12 @@ let UserSchema = new Schema({
 UserSchema.pre('save', function(next) {
   let user = this;
 //only hash the password if it is new or modified
-  if (!User.isModified('password')) return next();
+  if (!user.isModified('password')) return next();
   //generate salt (Yo mamas so fat Thanos had to snap twice)
   bcrypt.genSalt(saltRounds, function(err, salt) {
     if (err) return next(err);
     //hash the password using our new salt
-    bcrypt.hash(User.password, salt, function(err, hash) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
       if (err) return next(err);
       //override the cleartext password with the hashed one
       user.password = hash;
